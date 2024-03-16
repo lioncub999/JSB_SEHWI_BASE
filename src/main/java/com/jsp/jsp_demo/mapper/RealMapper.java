3902a46b4e384170be0f89dd3a532feb8aeb50f5
sehwing <lioncub999@naver.com>
2024-03-16 17:02:51 +0900

    test

---
+++ src/main/java/com/jsp/jsp_demo/mapper/RealMapper.java
@@ -0,0 +1,9 @@
+package com.jsp.jsp_demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RealMapper {
+
+    int getRealNum();
+}
---
+++ src/test/java/com/jsp/jsp_demo/JspDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.jsp_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
